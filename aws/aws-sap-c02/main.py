1b7d1fcc3c9ee24afcc869cdcfd2ab3fac66262a
Louis Wan <chwanlouis@gmail.com>
2025-07-11 00:18:26 +0800

    add study card app

--- aws/aws-sap-c02/main.py
+++ aws/aws-sap-c02/main.py
@@ -1,10 +1,21 @@
-import time
-import random
-import urllib.request
+
+
+
+
+
+import json
+
+
+class AWSQuiz(object):
+    @staticmethod
+    def print_question(data: dict) -> None:
+        return
+
+
 
 
 if __name__ == "__main__":
-    url_head = "https://free-braindumps.com/amazon/free-aws-solutions-architect-professional-braindumps.html?p="
-    for i in range(65, 112):
-        urllib.request.urlretrieve(f"{url_head}{i+1}", f"webpage\\page_{i}.html")
-        time.sleep(random.randint(10, 20))
+    file_name = "jsons\\qdb.json"
+    with open(file_name, encoding="utf-8") as file:
+        qdb = json.load(file)
+    print(qdb[0]["question"])
